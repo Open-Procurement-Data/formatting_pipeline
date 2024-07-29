8fd0749a2f9b29c80102e3ddc925d283ab454ecd
LumiLink5 <tetiana.prykhodko@dataport.de>
2024-07-29 12:27:01 +0200

    pipeline files

---
+++ new_dataframes.py
@@ -0,0 +1,163 @@
+import os
+import pandas as pd
+import sys
+from datetime import datetime
+
+def get_cpv(cpv_input, dir_output, printing):
+    '''
+    Beginning Logs for more info.
+    Checking if the given paths are correct.
+    Reading the cpv_numbers
+    '''
+    # check for output dir / create one if it does not exist
+    try:
+        os.makedirs(dir_output, exist_ok=True)
+        if printing is True:
+            print(f"Output directory created or already exists: {dir_output}")
+    except Exception as e:
+        if printing is True:
+            print(f"Error creating output directory: {e}")
+        sys.exit(1)
+
+    try:
+        cvp_numbers = pd.read_excel(cpv_input, usecols=['CODE', 'DE'])
+        if printing is True:
+            print(f"Processed {cpv_input}")
+    except Exception as e:
+        if printing is True:
+            print(f"Error processing file {cpv_input}: {e}")
+
+    return cvp_numbers
+
+def extract_codes(df, code_column): 
+    """
+    Extract divisions, groups, classes, and categories from the given DataFrame based on the code rules.
+    
+    Parameters:
+    df (pd.DataFrame): Input DataFrame containing the codes and descriptions.
+    code_column (str): Column name containing the codes.
+    """
+    def get_division(code):
+        return code[:2]
+    
+    def get_group(code):
+        return code[:3]
+    
+    def get_class(code):
+        return code[:4]
+    
+    def get_category(code):
+        return code[:5]
+    
+    def get_classification(code):
+        code = code.split('-')[0]
+        if code.endswith('000000'):
+            return 'division'
+        elif code.endswith('00000'):
+            return 'group'
+        elif code.endswith('0000'):
+            return 'class'
+        elif code.endswith('000'):
+            return 'category'
+        else:
+            return 'subclass'
+        
+    df['division'] = df[code_column].apply(get_division)
+    df['group'] = df[code_column].apply(get_group)
+    df['class'] = df[code_column].apply(get_class)
+    df['category'] = df[code_column].apply(get_category)
+    #definetly needed, the later 4 lines are obsolete (maybe)
+    df['classification'] = df[code_column].apply(get_classification) 
+    
+    return df
+
+def get_classification(cpv_numbers_list, cvp_numbers, classification = "division"):
+    kurzel = []
+    intermediate_cvp = cvp_numbers[cvp_numbers['classification'] == classification] # or 'division', 'group', 'class', 'category', 'subclass'
+
+    for cpv_number in cpv_numbers_list:
+        if pd.notna(cpv_number):
+            for i in intermediate_cvp["CODE"]:
+                if i[:2] == str(cpv_number)[:2]:
+                    division_desc = intermediate_cvp[intermediate_cvp["CODE"] == i]["DE"].values[0]
+                    kurzel.append(division_desc)
+    return list(set(kurzel))
+
+def save_new_files(dataframe, name, dir_output, printing):
+    '''
+    Saving the reformatted pandas.DataFrame to a csv.
+    Only if a output directory path was given.
+    '''
+    if dir_output is None:
+        if printing is True:
+            print("No output directory specified. Skipping saving the DataFrame.")
+        return
+    
+    if isinstance(dataframe, pd.DataFrame):
+        current_date = datetime.now().strftime("%Y_%m_%d")
+        csv_file_name = f"{current_date}_{name}.csv"
+        csv_file_path = os.path.join(dir_output, csv_file_name)
+        try:
+            dataframe.to_csv(csv_file_path, index=False)
+            if printing is True:
+                print(f"Saved DataFrame to {csv_file_path}")
+        except Exception as e:
+            if printing is True:
+                print(f"Error saving DataFrame to CSV: {e}")
+            sys.exit(1)
+    else:
+        if printing is True:
+            print(f"Invalid DataFrame for key {name}.")
+        sys.exit(1)
+
+def get_new_dataframes(dataframes, cpv_input_dir, output_dir=None, printing=False):
+    
+    if not isinstance(printing, bool):
+        raise ValueError("The 'printing' parameter must be a boolean value.")
+    
+    cvp_numbers = get_cpv(cpv_input_dir, output_dir, printing)
+
+    df = extract_codes(cvp_numbers, 'CODE')
+
+    dataframes["overView_Ted"]["classification"] = dataframes["overView_Ted"]["classification-cpv"].apply(lambda x: get_classification(x, cvp_numbers) if isinstance(x, list) else [])
+
+    bescha_new = pd.DataFrame()
+    ted_new = pd.DataFrame()
+
+    # Equal column names for multiple DataFrames:
+
+    bescha_new["tender_title"] = dataframes["overView_Bescha"]["tender.title"].copy()
+    ted_new["tender_title"] = dataframes["overView_Ted"]["notice-title"].copy()
+
+    bescha_new["tender_description"] = dataframes["overView_Bescha"]["tender.description"].copy()
+    ted_new["tender_description"] = dataframes["overView_Ted"]["description-lot"].copy() 
+
+    bescha_new["tender_cpv_number"] = dataframes["overView_Bescha"]["classification.id_tender.items_1"].copy()
+    ted_new["tender_cpv_number"] = dataframes["overView_Ted"]["classification-cpv"].copy() 
+
+    bescha_new["tender_cpv_category"] = dataframes["overView_Bescha"]["tender.mainProcurementCategory"].copy()
+    ted_new["tender_cpv_category"] = dataframes["overView_Ted"]["classification"].copy()
+
+    bescha_new["tender_numberOfTenderers"] = dataframes["overView_Bescha"]["tender.numberOfTenderers"].copy()
+    #ted_new["tender_numberOfTenderers"] = dataframes["overView_Ted"]["value.amount_contracts_1"].copy() # No column for that!
+
+    bescha_new["buyer_name"] = dataframes["overView_Bescha"]["tender.procuringEntity.name"].copy()
+    ted_new["buyer_name"] = dataframes["overView_Ted"]["organisation-name-buyer"].copy() 
+
+    bescha_new["buyer_locality"] = dataframes["overView_Bescha"]["buyer.address.locality"].copy()
+    ted_new["buyer_locality"] = dataframes["overView_Ted"]["buyer-city"].copy() 
+
+    bescha_new["buyer_nut"] = dataframes["overView_Bescha"]["buyer.address.region"].copy() 
+    ted_new["buyer_nut"] = dataframes["overView_Ted"]["buyer-country-sub"].copy() 
+
+    bescha_new["contracts_value_amount"] = dataframes["overView_Bescha"]["value.amount_contracts_1"].copy()
+    ted_new["contracts_value_amount"] = dataframes["overView_Ted"]["total-value"].copy() 
+
+    if printing is True:
+        print(f"bescha_new has following columns: {bescha_new.keys()}")
+        print(f"ted_new has following columns: {ted_new.keys()}")
+
+    save_new_files(bescha_new, "bescha", output_dir, printing)
+    save_new_files(ted_new, "ted", output_dir, printing)
+
+    return bescha_new, ted_new
---
+++ pipeline.py
@@ -0,0 +1,194 @@
+import json
+import os
+import re
+import pandas as pd
+import sys
+import time
+from datetime import datetime
+
+def check_dir(directory, printing):
+    '''
+    Checking if rhe given paths are correct
+    '''
+    try:
+        dir = directory
+        if not os.path.isdir(dir):
+            raise ValueError(f"Input directory does not exist: {dir}")
+        if printing is True:
+            print(f"Input directory exists: {dir}")
+        return dir
+    except Exception as e:
+        if printing is True:
+            print(f"Error accessing input directory: {e}")
+        sys.exit(1)
+
+def start_logs(dir_input, dir_output, printing):
+    '''
+    Beginning Logs for more info.
+    Checking if the given paths are correct.
+    Reading the cpv_numbers
+    '''
+    # check for output dir / create one if it does not exist
+    try:
+        os.makedirs(dir_output, exist_ok=True)
+        if printing is True:
+            print(f"Output directory created or already exists: {dir_output}")
+    except Exception as e:
+        if printing is True:
+            print(f"Error creating output directory: {e}")
+        sys.exit(1)
+
+    # check input dir
+    data_dir = check_dir(dir_input, printing)
+
+    return data_dir
+
+def save_new_files(dataframes, dir_output, printing):
+    '''
+    Saving the reformatted pandas.DataFrame to a csv.
+    Only if a output directory path was given.
+    '''
+    if dir_output is None:
+        if printing is True:
+            print("No output directory specified. Skipping saving the DataFrame.")
+        return
+    
+    if dataframes is not None and isinstance(dataframes, dict):
+        for frame_name, frame in dataframes.items():
+            if isinstance(frame, pd.DataFrame):
+                current_date = datetime.now().strftime("%Y_%m_%d")
+                # CSV
+                csv_file_name = f"{current_date}_{frame_name}.csv"
+                csv_file_path = os.path.join(dir_output, csv_file_name)
+                try:
+                    frame.to_csv(csv_file_path, index=False)
+                    if printing is True:
+                        print(f"Saved DataFrame to {csv_file_path}")
+                except Exception as e:
+                    if printing is True:
+                        print(f"Error saving DataFrame to CSV: {e}")
+                    sys.exit(1)
+                # JSON
+                json_file_name = f"{current_date}_{frame_name}.json"
+                json_file_path = os.path.join(dir_output, json_file_name)
+                try:
+                    frame.to_json(json_file_path, orient='records')
+                    if printing is True:
+                        print(f"Saved DataFrame to {json_file_path}")
+                except Exception as e:
+                    if printing is True:
+                        print(f"Error saving DataFrame to JSON: {e}")
+                    sys.exit(1)
+            else:
+                if printing is True:
+                    print(f"Invalid DataFrame for key {frame_name}.")
+                sys.exit(1)
+    else:
+        if printing is True:
+            print("Invalid dictionary provided.")
+        sys.exit(1)
+
+
+def json_files_to_dataframes(directory):
+    '''
+    Returning a Dictionary of all pandas.DataFrames from one directory
+    '''
+    dataframes = {}
+    for filename in os.listdir(directory):
+        if filename.endswith(".json"):
+            json_name = os.path.splitext(filename)[0]
+            file_path = os.path.join(directory, filename)
+            with open(file_path, 'r') as file:
+                data = json.load(file)
+                dataframes[json_name] = pd.DataFrame(data)
+    return dataframes
+
+
+def extract_column(df, column_name):
+    '''
+    Extracting one specific column
+    '''
+    df_copy = df.copy()
+
+    exploded_parties_df = df_copy.explode(column_name)
+    
+    exploded_parties_df['parent_id'] = exploded_parties_df.index
+    
+    normalized_parties_df = pd.json_normalize(exploded_parties_df[column_name])
+
+    combined_df = exploded_parties_df.reset_index(drop=True).join(normalized_parties_df.add_suffix('_' + column_name))
+    
+    combined_df['parent_id'] = exploded_parties_df['parent_id'].values
+    
+    combined_df.drop(columns=[column_name], inplace=True)
+    
+    combined_df['suffix'] = combined_df.groupby('parent_id').cumcount() + 1
+    
+    combined_df.set_index(['parent_id', 'suffix'], inplace=True)
+
+    combined_df = combined_df.unstack().sort_index(level=1, axis=1)
+    
+    combined_df.columns = [f'{col[0]}_{col[1]}' for col in combined_df.columns]
+    
+    combined_df.reset_index(inplace=True)
+    
+    return combined_df
+
+def formatting_dataframe(df, list_of_columns, printing):
+    '''
+    formatting a whole pandas.DataFrame
+    '''
+    exploded_df = df.explode('releases')
+
+    result_df = pd.json_normalize(exploded_df['releases'])
+
+    new_list = ['_' + kw for kw in list_of_columns]
+    for column in list_of_columns:
+        if printing is True:
+            print(f"Starting extraction for column: {column}")
+
+        start_time = time.time()
+        result_df = extract_column(result_df, column)
+        end_time = time.time()
+
+        elapsed_time = end_time - start_time
+        if printing is True:
+            print(f"Time taken for extract_2 with column {column}: {elapsed_time:.2f} seconds")
+
+        pattern = re.compile(r'^_[2-9]|\d_{2,}')
+
+        columns_to_drop = [col for col in result_df.columns if pattern.search(col) and not any(keyword in col for keyword in new_list)]
+        
+        result_df = result_df.drop(columns=columns_to_drop)
+
+        result_df.columns = [col[:-2] if ('_' + column) not in col else col for col in result_df.columns]
+
+        result_df.columns = [col.rstrip('_') for col in result_df.columns]
+
+        result_df = result_df.loc[:,~result_df.columns.duplicated()].copy()
+    
+    return result_df
+
+
+def get_dataframes(data_dir, output_dir=None, printing=False):
+    '''
+    Processes data and returns DataFrames.
+    '''
+
+    if not isinstance(printing, bool):
+        raise ValueError("The 'printing' parameter must be a boolean value.")
+    
+    data_dir = start_logs(data_dir, output_dir, printing)
+
+    dataframes = json_files_to_dataframes(data_dir)
+
+    first_layer_columns = ['parties', 'awards', 'contracts', 'tender.items', 'tender.lots']
+
+    dataframes["overView_Bescha"] = formatting_dataframe(dataframes["overView_Bescha"], first_layer_columns, printing)
+
+    if printing is True:
+        print(f"Bescha shape: {dataframes['overView_Bescha'].shape}")
+
+    save_new_files(dataframes, output_dir, printing)
+
+    return dataframes
